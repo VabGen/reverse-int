b9ac76b3b13cc289dab8f93064620cf7c96628c0
VabGen <vabgen@gmail.com>
2022-08-11 20:38:49 +0300

    fix: declaration of constants in a file index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let number = Math.abs(n);
+    let array = number.toString().split('');
+    let revers = array.reverse()
+    revers = revers.join('')
+    return Number(revers);
 }
